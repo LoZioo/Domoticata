300f8035df2a99b8ed728fe990ef066215fcc483
LoZioo <60042676+LoZioo@users.noreply.github.com>
2024-07-27 18:36:14 +0200

    feat: unilibc configurations.

---
+++ src/wall_terminal/include/ul_configs.h
@@ -0,0 +1,21 @@
+/** @file ul_configs.h
+ *  @brief  Created on: June 30, 2024
+ *          Davide Scalisi
+ *
+ * 					Description:	DS framework configurations.
+ *
+ * @copyright [2024] Davide Scalisi *
+ * @copyright All Rights Reserved. *
+ *
+*/
+
+#ifndef INC_UL_CONFIGS_H_
+#define INC_UL_CONFIGS_H_
+
+/************************************************************************************************************
+* ul_errorss.h
+************************************************************************************************************/
+
+// #define UL_CONF_PRINT_DEBUG										// Comment to disable the text debug output.
+
+#endif  /* INC_UL_CONFIGS_H_ */
