2cd5f854c02738a9cd42e00355243ac6310e81fe
LoZioo <60042676+LoZioo@users.noreply.github.com>
2024-07-30 19:04:01 +0200

    fix

--- src/wall_terminal/lib/button_states/button_states.c
+++ src/wall_terminal/lib/button_states/button_states.c
@@ -32,7 +32,7 @@ static uint16_t __button_states = 0;
  ************************************************************************************************************/
 
 static uint8_t __btn_bit_shift(button_id_t button);
-static uint8_t __btn_bit_mask(button_id_t button);
+static uint16_t __btn_bit_mask(button_id_t button);
 
 /************************************************************************************************************
 * Private Functions Definitions
@@ -42,7 +42,7 @@ uint8_t __btn_bit_shift(button_id_t button){
 	return (button * 2 - 2);
 }
 
-uint8_t __btn_bit_mask(button_id_t button){
+uint16_t __btn_bit_mask(button_id_t button){
 	return (3 << __btn_bit_shift(button));
 }
 
@@ -52,19 +52,15 @@ uint8_t __btn_bit_mask(button_id_t button){
 
 button_state_t get_button_state(button_id_t button){
 	return (
-		(
-			__button_states &
-			__btn_bit_mask(button)
-		) >> __btn_bit_shift(button)
+		( __button_states & __btn_bit_mask(button) ) >>
+		__btn_bit_shift(button)
 	);
 }
 
 void set_button_state(button_id_t button, button_state_t state){
 	__button_states = (
-		(
-			__button_states &
-			~( (uint16_t) __btn_bit_mask(button) )
-		) | ( state << __btn_bit_shift(button) )
+		( __button_states & ~__btn_bit_mask(button) ) |
+		( state << __btn_bit_shift(button) )
 	);
 }
 
