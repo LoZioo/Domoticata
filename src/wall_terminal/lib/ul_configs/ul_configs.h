a537d44e3efaf2441988fbb6bc97d4b9f1e758d8
LoZioo <60042676+LoZioo@users.noreply.github.com>
2024-08-02 09:39:25 +0200

    chore

--- src/wall_terminal/lib/ul_configs/ul_configs.h
+++ src/wall_terminal/lib/ul_configs/ul_configs.h
@@ -16,6 +16,12 @@
 * ul_errors.h
 ************************************************************************************************************/
 
-// #define UL_CONF_ERRORS_PRINT_DEBUG										// Comment to disable the text debug output.
+// #define UL_CONF_ERRORS_PRINT_DEBUG					// Comment to disable the text debug output.
+
+/************************************************************************************************************
+* ul_master_slave.h
+************************************************************************************************************/
+
+// #define UL_CONF_MS_UINT8_IS_BUF_SIZE_T			// Comment to set the max buffer sizes from (2^32)-1 to (2^8)-1 and save some RAM.
 
 #endif  /* INC_UL_CONFIGS_H_ */
