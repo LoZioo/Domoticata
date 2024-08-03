6a25f34fbd2ae4d2e8658a1a9e78eda2d24a5cdd
LoZioo <60042676+LoZioo@users.noreply.github.com>
2024-08-03 13:05:01 +0200

    feat: code boilerplate.

---
+++ src/control_unit/main/main.c
@@ -0,0 +1,96 @@
+/* USER CODE BEGIN Header */
+/**
+	******************************************************************************
+	* @file           : main.c
+	* @brief          : main program body.
+	******************************************************************************
+	* @attention
+	*
+	* Copyright (c) [2024] Davide Scalisi *
+	* All rights reserved.
+	*
+	* This software is licensed under terms that can be found in the LICENSE file
+	* in the root directory of this software component.
+	* If no LICENSE file comes with this software, it is provided AS-IS.
+	*
+	******************************************************************************
+	*/
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+// Standard libraries.
+
+// Platform libraries.
+#include <freertos/FreeRTOS.h>
+
+// UniLibC libraries.
+// extern "C" {
+// 	#include <ul_errors.h>
+// }
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+ * @brief App entry point.
+*/
+void app_main(){
+
+	/* MCU Configuration--------------------------------------------------------*/
+	/* USER CODE BEGIN SysInit */
+
+	/* USER CODE END SysInit */
+
+	/* USER CODE BEGIN Init */
+
+	/* USER CODE END Init */
+
+	/* USER CODE BEGIN 1 */
+
+	/* Infinite loop */
+	for(;;){
+		vTaskDelay(pdMS_TO_TICKS(1));
+	}
+	/* USER CODE END 1 */
+}
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 2 */
+
+/* USER CODE END 2 */
+
+/* Private user code for ISR (Interrupt Service Routines) --------------------*/
+/* USER CODE BEGIN ISR */
+
+/* USER CODE END ISR */
