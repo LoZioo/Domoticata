f452b158f7cad22a7fec883223c09450ab8caa03
LoZioo <60042676+LoZioo@users.noreply.github.com>
2024-08-03 14:44:47 +0200

    chore

--- src/wall_terminal/platformio_install_unilibc.sh
+++ src/wall_terminal/platformio_install_unilibc.sh
@@ -3,6 +3,7 @@ set -e
 
 FILENAME="unilibc.txt"
 REPO_URL="git@github.com:LoZioo/UniLibC.git"
+TARGET="lib"
 
 rm -rf tmp
 
@@ -16,14 +17,15 @@ fi
 mapfile -t libs < <(sed 's/\r$//' "$FILENAME")
 
 git clone $REPO_URL tmp
-mkdir -p lib
+
+mkdir -p "$TARGET"
 
 for i in "${!libs[@]}"; do
 
 	# Do not overwrite the `ul_configs` lib but, if does not exist, use the default one.
-	if [[ "${libs[$i]}" != "configs" || ( "${libs[$i]}" == "configs" && ! -e "lib/ul_configs" ) ]]; then
-		rm -rf "lib/ul_${libs[$i]}"
-		mv "tmp/lib/ul_${libs[$i]}" lib
+	if [[ "${libs[$i]}" != "configs" || ( "${libs[$i]}" == "configs" && ! -e "$TARGET/ul_configs" ) ]]; then
+		rm -rf "$TARGET/ul_${libs[$i]}"
+		mv "tmp/lib/ul_${libs[$i]}" "$TARGET"
 	fi
 done
 
